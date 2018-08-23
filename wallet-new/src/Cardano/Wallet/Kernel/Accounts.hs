ce0e2321a6d38363a590ccf3f7451e813315bdb2
Johannes Lund <johannes.lund@iohk.io>
2018-08-23 17:50:33 +0200

    [CO-374] Make CreateAccountError an WalletException
    Stub implementation to see if it works out

--- wallet-new/src/Cardano/Wallet/Kernel/Accounts.hs
+++ wallet-new/src/Cardano/Wallet/Kernel/Accounts.hs
@@ -14,10 +14,15 @@ import qualified Formatting as F
 import qualified Formatting.Buildable
 import           System.Random.MWC (GenIO, createSystemRandom, uniformR)
 
+import           Servant (err400)
+
 import           Data.Acid (update)
+import           Data.Aeson (ToJSON, toJSON)
 
 import           Pos.Crypto (EncryptedSecretKey, PassPhrase)
 
+import           Cardano.Wallet.API.V1.Errors (ToHttpErrorStatus (..),
+                     ToServantError (..))
 import           Cardano.Wallet.Kernel.DB.AcidState (CreateHdAccount (..), DB,
                      DeleteHdAccount (..), UpdateHdAccountName (..))
 import           Cardano.Wallet.Kernel.DB.HdWallet (AccountName (..),
@@ -32,6 +37,8 @@ import           Cardano.Wallet.Kernel.Internal (PassiveWallet, walletKeystore,
                      wallets)
 import qualified Cardano.Wallet.Kernel.Keystore as Keystore
 import           Cardano.Wallet.Kernel.Types (WalletId (..))
+import           Cardano.Wallet.Kernel.WalletException
+                     (walletExceptionFromException, walletExceptionToException)
 
 import           Test.QuickCheck (Arbitrary (..), oneof)
 
@@ -58,10 +65,18 @@ instance Buildable CreateAccountError where
     build (CreateAccountHdRndAccountSpaceSaturated hdAcc) =
         bprint ("CreateAccountHdRndAccountSpaceSaturated " % F.build) hdAcc
 
+instance ToJSON CreateAccountError where
+    toJSON = error "TODO"
+instance ToServantError CreateAccountError where
+    declareServantError _ = err400 -- FIXME: picked by random for now !!!!!!!!!!
+instance ToHttpErrorStatus CreateAccountError
+
 instance Show CreateAccountError where
     show = formatToString build
 
-instance Exception CreateAccountError
+instance Exception CreateAccountError where
+    toException   = walletExceptionToException
+    fromException = walletExceptionFromException
 
 -- | Creates a new 'Account' for the input wallet.
 -- Note: @it does not@ generate a new 'Address' to go in tandem with this
